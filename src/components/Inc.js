e9136420ee89dec2248b562a9fe63f46ec36c0d3
uanoob <usnet@ukr.net>
2020-04-17 10:59:46 +0300

    Added minValue, maxValue, step parameters to useInc hook

--- src/components/Inc.js
+++ src/components/Inc.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { useInc } from '../hooks/useInc';
 
 const Inc = () => {
-  const [count, { decrease, increase }] = useInc(0);
+  const [count, { decrease, increase, reset }] = useInc({
+    initial: 5,
+    step: 2,
+    minValue: 0,
+    maxValue: 10,
+  });
   return (
     <div>
+      <h3>Volume</h3>
       <button onClick={decrease}>-</button>
       {count}
       <button onClick={increase}>+</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
--- src/hooks/useInc.js
+++ src/hooks/useInc.js
@@ -1,8 +1,20 @@
 import { useState } from 'react';
 
-export const useInc = (initial = 0) => {
+export const useInc = ({
+  maxValue = 100,
+  minValue = -100,
+  initial = 0,
+  step = 1,
+}) => {
   const [value, setValue] = useState(initial);
-  const increase = () => setValue(prevState => prevState + 1);
-  const decrease = () => setValue(prevState => prevState - 1);
-  return [value, { increase, decrease }];
+  const increase = () =>
+    setValue(prevState =>
+      prevState + step > maxValue ? maxValue : prevState + step,
+    );
+  const decrease = () =>
+    setValue(prevState =>
+      prevState - step < minValue ? minValue : prevState - step,
+    );
+  const reset = () => setValue(initial);
+  return [value, { increase, decrease, reset }];
 };
